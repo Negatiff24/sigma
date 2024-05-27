c2134dcbcc6733bd93285e060e272807de61f8ed
Vanya Goncharov <negatiff24@mail.ru>
2024-05-28 02:05:47 +0300

    acc sheet

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,11 @@
-main.js
+function goToIndex() {
+    window.location.href = '../index.html'
+}
+
+function goToCatalog() {
+    window.location.href = 'sheets/catalog.html'
+}
+
+function goToAcc() {
+    window.location.href = 'sheets/acc.html'
+}
